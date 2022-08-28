3eeebc7c0d8e6c4817a6d6600cd9854d4f30d701
Jeffrey Onochie <jeff.ict@gmail.com>
2022-08-28 23:32:07 +0100

    Add basic graphql Query

--- server.js
+++ server.js
@@ -4,21 +4,75 @@ const {graphqlHTTP} = require('express-graphql');
 
 const schema = buildSchema(`
     type Query {
+        products: [Product]
+        orders: [Order]
+    }
+
+    type Product {
+        id: ID!
+        title: String!
         description: String
-        price: Float
+        reviews: [Review]
+        price: Float!
+    }
+
+    type Review {
+        rating: Int!
+        comment: String!
+    }
+
+    type Order {
+        date: String!
+        subtotal: Float!
+        items: [OrderItem]
+    }
+
+    type OrderItem {
+        product: Product!
+        quantity: Int!
     }
 `);
 
 const root = {
-    description: 'Red Shoe',
-    price: 42.12
-}
+    products: [
+        {
+            id: 'redshoe',
+            title: 'Red Shoe',
+            description: 'A red smart shoe',
+            price: 42.12
+        },
+        {
+            id: 'bluejeans',
+            title: 'Blue Jeans',
+            description: 'A blue smart jeans',
+            price: 55.55
+        },
+    ],
+    orders: [
+        {
+            date: '2008-07-27',
+            subtotal: 90.22,
+            items: [
+                {
+                    product: {
+                        id: 'redshoe',
+                        title: 'Red Shoe',
+                        description: 'A red smart shoe',
+                        price: 42.12
+                    },
+                    quantity: 2
+                }
+            ]
+        }
+    ] 
+};
 
 const app = express();
 
 app.use('/graphql', graphqlHTTP({
     schema,
-    rootValue: root
+    rootValue: root,
+    graphiql: true
 }));
 
 app.listen(3000, () => console.log('Running GraphQL server...'));
